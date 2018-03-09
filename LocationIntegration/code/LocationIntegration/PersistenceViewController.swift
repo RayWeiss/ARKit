4ca113f47da9f17577e01398c865b2f62a45cd07
rweiss8 <rweiss8>
2018-03-09 17:15:25 +0000

    persistenceViewController saves and loads node being tracked
    git-svn-id: https://svn.cs.kent.edu/18S-iOS_Project/svn@68 37b6f9cb-bdff-49da-9f5d-9f769ffbf1d4

--- LocationIntegration/code/LocationIntegration/PersistenceViewController.swift
+++ LocationIntegration/code/LocationIntegration/PersistenceViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import SceneKit
 class PersistenceViewController: UIViewController {
     
     var arSceneViewController: ARSceneViewController!
@@ -17,6 +17,59 @@ class PersistenceViewController: UIViewController {
         self.addSwipeGesture()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    // MARK: Save / Load Buttons
+    @IBAction func saveButtonTapped(_ sender: UIButton) {
+        print("save tapped")
+        guard let nodeToSave = self.arSceneViewController.getWaypointBeingTracked() else { return }
+        self.save(node: nodeToSave)
+    }
+    
+    @IBAction func loadButtonTapped(_ sender: UIButton) {
+        print("load tapped")
+        self.load()
+    }
+    
+    // MARK: Save
+    func getArchiveURL () -> NSURL {
+        // Get the default file manager
+        let fileManager = FileManager()// NSFileManager.defaultManager()
+        // Get an array of URLs
+        let urls = fileManager.urls(for: .documentDirectory,
+                                    in: .userDomainMask)
+        // Get the document directory
+        let documentDirectory = urls.last
+        let fileWithPath = documentDirectory?.appendingPathComponent("archive.data")
+        // Debug output
+        print(">>>Document Directory: \(documentDirectory!)")
+        return fileWithPath! as NSURL
+    }
+    
+    func save(node: SCNNode) {
+        let archiveFile = self.getArchiveURL().path!
+        let success = NSKeyedArchiver.archiveRootObject(node, toFile: archiveFile)
+        if !success { print(">>> Archive failed.") }
+    }
+    
+    // MARK: Load
+    func load() {
+        let archiveFile = self.getArchiveURL().path!
+        // The file will not exist the first time the app is run
+        if !FileManager().fileExists(atPath: archiveFile) {
+            print(">>> Does not exist: \(archiveFile)")
+            return
+        }
+        //Debugging
+        print(">>> archiveURL: \(archiveFile)")
+        // Get the archived data
+        let unArchivedData = NSKeyedUnarchiver.unarchiveObject(withFile: archiveFile)
+        let unArchivedNode = unArchivedData as? SCNNode
+        guard let loadedNode = unArchivedNode else { print(">>> Unarchive failed."); return }
+        self.arSceneViewController.arSceneView.scene.rootNode.addChildNode(loadedNode)
+    }
     
     // MARK: Swipe Gesture
     func addSwipeGesture() {
